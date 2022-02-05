6f2c7594bceac0608f485168537e58e131812142
MmorenoAH <mmarcosrock@gmail.com>
2022-02-05 16:21:59 -0600

    Agregar archivos de proyecto.

---
+++ ElOrientalVirtualMarcoMoreno/Models/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElOrientalVirtualMarcoMoreno.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(100)]
+        [Required(ErrorMessage ="El Nombre del Producto es requerido.")]
+        public string Nombre { get; set; }
+        [StringLength()]
+        public double Precio { get; set; }
+        public string Descripcion { get; set; }
+        public string Ruta { get; set; }
+
+    }
+}
