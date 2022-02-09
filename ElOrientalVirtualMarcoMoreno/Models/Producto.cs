a484be961a47630cbc76e218151ca58a3900336d
MmorenoAH <99095367+MmorenoAH@users.noreply.github.com>
2022-02-09 03:34:48 -0600

    Update Producto.cs

--- ElOrientalVirtualMarcoMoreno/Models/Producto.cs
+++ ElOrientalVirtualMarcoMoreno/Models/Producto.cs
@@ -5,14 +5,14 @@ namespace ElOrientalVirtualMarcoMoreno.Models
     public class Producto
     {
         [Key]
-        public int Id { get; set; }
+        public int IdProducto { get; set; }
         [StringLength(100)]
         [Required(ErrorMessage ="El Nombre del Producto es requerido.")]
-        public string Nombre { get; set; }   
-        public double Precio { get; set; }
+        public string NombreProducto { get; set; }   
+        public double PrecioProducto { get; set; }
         [StringLength(500)]
         [Required(ErrorMessage ="La descripcion es requerida.")]
-        public string Descripcion { get; set; }
+        public string DescripcionProducto { get; set; }
         [StringLength(1000)]
         public string Ruta { get; set; }
 
