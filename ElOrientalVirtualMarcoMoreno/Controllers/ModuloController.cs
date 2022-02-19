615feb553af0dd3207743e050244054061ebfb92
MmorenoAH <mmarcosrock@gmail.com>
2022-02-19 02:15:49 -0600

    confiuracion de vista y agregar categoria pendientes...

---
+++ ElOrientalVirtualMarcoMoreno/Controllers/ModuloController.cs
@@ -0,0 +1,23 @@
+﻿using ElOrientalVirtualMarcoMoreno.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElOrientalVirtualMarcoMoreno.Controllers
+{
+    public class ModuloController : Controller
+    {
+        private readonly MyDbContext _context;
+        public ModuloController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
