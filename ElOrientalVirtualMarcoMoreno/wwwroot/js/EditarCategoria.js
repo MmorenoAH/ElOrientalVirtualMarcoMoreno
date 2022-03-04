babdbf73e82cb82d6657b4468472e9aad7a99e35
MmorenoAH <99095367+MmorenoAH@users.noreply.github.com>
2022-03-04 04:07:19 -0600

    Add files via upload
    Archivo para editar Categorias

---
+++ ElOrientalVirtualMarcoMoreno/wwwroot/js/EditarCategoria.js
@@ -0,0 +1,53 @@
+$("#btnGuardar").click(function () {
+
+    var nombreCategoria = $(".Nombre").val();
+    var descripcionCatergoria = $(".Descripcion").val();
+
+    //validar que los campos sean requeridos
+    if (nombreCategoria == "" || descripcionCatergoria == "") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Todos los campos son requeridos',
+            color: 'white'
+        })
+    }
+    else {
+        //Hacer la perticion al servidor para guardar
+
+        //utilizar ajax
+        var datos = $.ajax({
+            //url de destino
+            url: "Crear",
+            type: "POST",
+            data: {
+                "NombreCategoria": nombreCategoria,
+                "DescripcionCategoria": descripcionCatergoria
+            }
+        })
+        //Mensaje de respuesta
+        datos.done(function (data) {
+            if (data.success) {
+                Swal.fire(
+                    'Buen trabajo!',
+                    data.message,
+                    'success'
+                )
+                $(".Nombre").val(""),
+                $(".Descripcion").val("")
+            } else {
+                Swal.fire(
+                    'Error',
+                    data.message,
+                    'error'
+                )
+            }
+        })
+        datos.fail(function () {
+            notif({
+                msg: "Error al guardar.",
+                type: "error"
+            });
+        })
+    }
+});
